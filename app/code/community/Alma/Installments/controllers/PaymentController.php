085bf5aaa19a8db92a6c01f21440bebf7cb7e3b2
Olivier Lance <olivier.lance@getalma.eu>
2019-01-14 15:30:56 +0100

    Make sure we don't try to validate a payment/order twice

--- app/code/community/Alma/Installments/controllers/PaymentController.php
+++ app/code/community/Alma/Installments/controllers/PaymentController.php
@@ -55,58 +55,73 @@ class Alma_Installments_PaymentController extends Mage_Core_Controller_Front_Act
 
         $order = Mage::getModel('sales/order')->load($almaPayment->custom_data["order_id"]);
         $payment = $order->getPayment();
+
         $quote = Mage::getModel('sales/quote')->load($almaPayment->custom_data["quote_id"]);
-
-        if (Alma_Installments_Helper_Functions::priceToCents($payment->getAmountAuthorized()) !== $almaPayment->purchase_amount) {
-            $internalError = $this->__(
-                "Paid amount (%1) does not match due amount (%2) for order %3",
-                Functions::priceFromCents($almaPayment->purchase_amount),
-                $payment->getAmountAuthorized(),
-                $order->getIncrementId()
-            );
-
-            $logger->error($internalError);
-            $this->cancelOrder($order, $internalError);
-
-            $this->getSession()->addError($errorMessage);
-            return $this->_redirect('checkout/cart');
-        }
-
-        $firstInstalment = $almaPayment->payment_plan[0];
-        if (!in_array($almaPayment->state, array(Payment::STATE_IN_PROGRESS, Payment::STATE_PAID)) || $firstInstalment->state !== Instalment::STATE_PAID) {
-            $internalError = $this->__(
-                "Payment state incorrect (%s & %s) for order %s",
-                $almaPayment->state,
-                $firstInstalment->state,
-                $order->getIncrementId()
-            );
-
-            $logger->error($internalError);
-            $this->cancelOrder($order, $internalError);
-
-            $this->getSession()->addError($errorMessage);
-            return $this->_redirect('checkout/cart');
-        }
-
-        // Register successful capture to update order state and generate invoice
-        $order->addStatusHistoryComment($this->__('First instalment captured successfully'));
-        $order->setCanSendNewEmailFlag(true);
-        $order->save();
-
-        // notify customer
-        $order->queueNewOrderEmail();
-        $order->setEmailSent(true);
-
-        $payment->registerCaptureNotification($payment->getBaseAmountAuthorized());
-
-
         $quoteId = $quote->getId();
-        $this->getSession()->setLastQuoteId($quoteId)->setLastSuccessQuoteId($quoteId);
+
+        $this->getSession()->setLastQuoteId($quoteId);
         $this->getSession()->setLastOrderId($order->getId())->setLastRealOrderId($order->getIncrementId());
 
-        $order->save();
+        if (in_array($order->getState(), array(Mage_Sales_Model_Order::STATE_NEW, Mage_Sales_Model_Order::STATE_PENDING_PAYMENT))) {
+            if (Alma_Installments_Helper_Functions::priceToCents($payment->getAmountAuthorized()) !== $almaPayment->purchase_amount) {
+                $internalError = $this->__(
+                    "Paid amount (%1) does not match due amount (%2) for order %3",
+                    Functions::priceFromCents($almaPayment->purchase_amount),
+                    $payment->getAmountAuthorized(),
+                    $order->getIncrementId()
+                );
 
-        return $this->_redirect('checkout/onepage/success', array('_secure'=>true));
+                $logger->error($internalError);
+                $this->cancelOrder($order, $internalError);
+
+                $this->getSession()->addError($errorMessage);
+                return $this->_redirect('checkout/onepage/failure/');
+            }
+
+            $firstInstalment = $almaPayment->payment_plan[0];
+            if (!in_array($almaPayment->state, array(Payment::STATE_IN_PROGRESS, Payment::STATE_PAID)) || $firstInstalment->state !== Instalment::STATE_PAID) {
+                $internalError = $this->__(
+                    "Payment state incorrect (%s & %s) for order %s",
+                    $almaPayment->state,
+                    $firstInstalment->state,
+                    $order->getIncrementId()
+                );
+
+                $logger->error($internalError);
+                $this->cancelOrder($order, $internalError);
+
+                $this->getSession()->addError($errorMessage);
+                return $this->_redirect('checkout/onepage/failure/');
+            }
+
+            // Register successful capture to update order state and generate invoice
+            $order->addStatusHistoryComment($this->__('First instalment captured successfully'));
+            $order->setCanSendNewEmailFlag(true);
+            $order->save();
+
+            // notify customer
+            $order->queueNewOrderEmail();
+            $order->setEmailSent(true);
+
+            $payment->registerCaptureNotification($payment->getBaseAmountAuthorized());
+
+            $this->getSession()->setLastSuccessQuoteId($quoteId);
+            $order->save();
+
+            return $this->_redirect('checkout/onepage/success', array('_secure'=>true));
+
+        } elseif ($order->getState() == Mage_Sales_Model_Order::STATE_CANCELED) {
+            $this->getSession()->addError($this->__('Your order has been canceled'));
+            return $this->_redirect('checkout/onepage/failure', array('_secure'=>true));
+
+        } elseif (in_array($order->getState(), array(Mage_Sales_Model_Order::STATE_PROCESSING, Mage_Sales_Model_Order::STATE_COMPLETE, Mage_Sales_Model_Order::STATE_HOLDED, Mage_Sales_Model_Order::STATE_PAYMENT_REVIEW))) {
+            $this->getSession()->setLastSuccessQuoteId($quoteId);
+            $order->save();
+
+            return $this->_redirect('checkout/onepage/success', array('_secure'=>true));
+        }
+
+        return $this->_redirect('checkout/cart', array('_secure'=>true));
     }
 
     /**
